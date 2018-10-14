d218ab7f772695f0757a2ab9fa67a16a0d174ac2
Gavin <gavin.n.lobo@gmail.com>
2018-10-14 12:05:08 -0400

    Added triangle and test cases

--- gcgs/triangle.h
+++ gcgs/triangle.h
@@ -1,59 +1,175 @@
 #pragma once
-#ifndef GCGS_TRIANGLE
-#define GCGS_TRIANGLE
+#ifndef CGCS_TRIANGLE_H
+#define CGCS_TRIANGLE_H
 
 #include <glm/glm.hpp>
+#include "hyperplane.h"
 
+#include <ostream>
+
+namespace gcgs
+{
+class triangle;
+}
+std::ostream & operator << (std::ostream & out, gcgs::triangle const & L);
+
+namespace gcgs
+{
+
+/**
+ * @brief The triangle class
+ *
+ * A line segment is a line connecting two points
+ * in two dimensions.
+ */
 class triangle
 {
-     public:
-        triangle()
+public:
+    using plane_type      = hyperplane<3>;
+    using point_type      = glm::vec3;
+
+    point_type m_point[3];
+
+    point_type & operator[](int i)
+    {
+        return m_point[i];
+    }
+    point_type const & operator[](int i) const
+    {
+        return m_point[i];
+    }
+
+    /**
+     * @brief get_hyperplane
+     * @return
+     *
+     * Returns the hyperplane represented by the line segment.
+     * The normal of the hyperplane is the vector p[1]-p[0] rotated
+     * 90 degrees counter-clockwise.
+     *
+     * This function is required for Tree
+     */
+    plane_type get_hyperplane() const
+    {
+        const auto v1 = m_point[1] - m_point[0];
+        const auto v2 = m_point[2] - m_point[0];
+        const auto n = glm::cross(v1,v2);
+        return plane_type( m_point[0], n );
+    }
+
+    /**
+     * @brief split
+     * @param plane
+     * @param inside
+     * @param outside
+     *
+     * Splits the line-segment using the plane. It returns a list of triangles which are
+     * below the plane and above the plane.
+     *
+     * This function is required for Tree
+     */
+    void split(const plane_type & plane, std::vector<triangle> & below, std::vector<triangle> & above) const
+    {
+        auto d0 = plane.distance( m_point[0]);
+        auto d1 = plane.distance( m_point[1]);
+        auto d2 = plane.distance( m_point[2]);
+
+
+
+        if( d0 <= 0 && d1 <= 0 && d2 <= 0 )
         {
+            below.push_back(*this);
+        }
+        else if( d0 >= 0 && d1 >=0 && d2 >= 0)
+        {
+            above.push_back(*this);
+        }
+        else
+        {
+            auto * p0 = &m_point[0];
+            auto * p1 = &m_point[1];
+            auto * p2 = &m_point[2];
+
+            while( !(std::signbit(d1) == std::signbit(d2) && std::signbit(d0)!=std::signbit(d1)) )
+            {
+                auto * tmp = p0;
+                p0 = p1;
+                p1 = p2;
+                p2 = tmp;
+
+                auto tmpd = d0;
+                d0 = d1;
+                d1 = d2;
+                d2 = tmpd;
+            }
+
+            // assume a is on one side
+            auto & a = *p0;
+            auto & b = *p1;
+            auto & c = *p2;
+
+            const auto v1 = a-b;
+            const auto t1 = glm::dot(  plane.m_normal, plane.m_point-a) / glm::dot( plane.m_normal, v1);
+            auto B = a + t1*v1;
+
+            const auto v2 = a-c;
+            const auto t2 = glm::dot(  plane.m_normal, plane.m_point-c) / glm::dot( plane.m_normal, v2);
+            auto C = a + t2*v2;
+            assert( !std::isnan(t1) );
+            assert( !std::isnan(t2) );
+
+            if( d0 < 0)
+            {
+                below.push_back( triangle{a,B,C});
+                above.push_back( triangle{c,C,B});
+                above.push_back( triangle{c,B,b});
+            }
+            else
+            {
+                above.push_back( triangle{a,B,C} );
+                below.push_back( triangle{c,C,B} );
+                below.push_back( triangle{c,B,b} );
+            }
 
         }
+    }
 
-        triangle(const glm::vec3 & a,const glm::vec3 & b, const glm::vec3 & c)
-        {
-            m_points[0]=a;
-            m_points[1]=b;
-            m_points[2]=c;
-        }
 
-        inline glm::vec3 & operator[](int i)
-        {
-            return m_points[i];
-        }
-        inline glm::vec3 const & operator[](int i) const
-        {
-            return m_points[i];
-        }
-        glm::vec3 normal() const
-        {
-            return glm::cross(  m_points[1]-m_points[0], m_points[2]-m_points[0] );
-        }
+    /**
+     * @brief intersection_point
+     * @param plane
+     * @return
+     *
+     * Returns the intersection point of this linesegment with the plane.
+     *
+     */
+    point_type intersection_point( plane_type const & plane) const
+    {
+        const auto v = m_point[1]-m_point[0];
+        //const auto d = plane.distance( m_point[0] );
+        const auto t = glm::dot(  plane.m_normal, plane.m_point-m_point[0]) / glm::dot( plane.m_normal, v);
+        return m_point[0] + t*v;
+    }
 
-        auto begin()
-        {
-            return m_points.begin();
-        }
 
-        auto end()
-        {
-            return m_points.end();
-        }
 
-        auto begin() const
-        {
-            return m_points.begin();
-        }
-
-        auto end() const
-        {
-            return m_points.end();
-        }
-
-        std::array<glm::vec3,3> m_points;
+    void print() const
+    {
+        std::cout << *this << std::endl;
+    }
 };
+
+
+
+}
+
+inline std::ostream & operator << (std::ostream & out, gcgs::triangle const & L)
+{
+    out << "(" << L[0].x << "," << L[0].y << ",";
+    out << L[1].x << "," << L[1].y << ")";
+    return out;
+}
+
 
 
 #endif
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -8,7 +8,8 @@
 
 #define GCGS_INCLUDE_TESTS
 #include <gcgs/line_segment.h>
-#include <gnl/gnl_arrayview.h>
+#include <gcgs/triangle.h>
+
 
 SCENARIO( "Line-Segment Hyperplanes" ) {
 
@@ -168,6 +169,99 @@ SCENARIO( "Splitting a Line-Segment" ) {
 
                 REQUIRE( fabs(Pcd.distance(outside[1][0]) ) == Approx(0.0f) );
                 REQUIRE( fabs(Pcd.distance(outside[1][1]) ) != Approx(0.0f) );
+            }
+        }
+    }
+}
+
+
+SCENARIO( "Triangle Hyperplanes" ) {
+
+    GIVEN( "A line segment ab and ba" )
+    {
+        glm::vec3 a( 1 ,  0 , 0);
+        glm::vec3 b(-1 ,  1 , 0);
+        glm::vec3 c(-1 , -1 , 0);
+
+        glm::vec3 outside(1,   4, 1);  // point inside the line
+        glm::vec3 inside(-15, -1, -1); // point outside the line
+
+        gcgs::triangle abc{a,b,c};
+
+        WHEN("The hyperplans are extracted")
+        {
+            auto h1 = abc.get_hyperplane();
+
+            THEN("The Normals are negative of each other")
+            {
+                REQUIRE( glm::length( h1.normal() - glm::vec3(0, 0, 1)) == Approx( 0.0 ));
+            }
+            THEN("Distance between the points and the hyerplans are zero")
+            {
+                REQUIRE( h1.distance(a) == Approx( 0.0 ));
+                REQUIRE( h1.distance(b) == Approx( 0.0 ));
+                REQUIRE( h1.distance(c) == Approx( 0.0 ));
+            }
+
+            THEN("Distance between the plane and the outside points are always positive")
+            {
+                REQUIRE( h1.distance(outside) == Approx(  1 ));
+                REQUIRE( h1.distance(inside) == Approx( -1 ));
+            }
+        }
+    }
+}
+
+
+
+
+SCENARIO( "Splitting a Triangle with Hyperplanes 2" ) {
+
+    GIVEN( "a hyperplane in the yz plane and a triangle abc" )
+    {
+        gcgs::hyperplane<3> splitting_plane( glm::vec3(0), glm::vec3(1,0,0));
+
+        glm::vec3 a( 1 ,  0 , 0);
+        glm::vec3 b(-1 ,  1 , 0);
+        glm::vec3 c(-1 , -1 , 0);
+
+        WHEN("A plane in the yz plane splits the triangle")
+        {
+            gcgs::triangle abc{a,b,c};
+
+            std::vector<gcgs::triangle> below, above;
+            abc.split(splitting_plane, below, above);
+
+            THEN("The triangle can be split into 3 triangles")
+            {
+                REQUIRE( below.size() == 2);
+                REQUIRE( above.size() == 1);
+            }
+        }
+        WHEN("A plane in the yz plane splits the triangle")
+        {
+            gcgs::triangle abc{b,c,a};
+
+            std::vector<gcgs::triangle> below, above;
+            abc.split(splitting_plane, below, above);
+
+            THEN("The triangle can be split into 3 triangles")
+            {
+                REQUIRE( below.size() == 2);
+                REQUIRE( above.size() == 1);
+            }
+        }
+        WHEN("A plane in the yz plane splits the triangle")
+        {
+            gcgs::triangle abc{c,a,b};
+
+            std::vector<gcgs::triangle> below, above;
+            abc.split(splitting_plane, below, above);
+
+            THEN("The triangle can be split into 3 triangles")
+            {
+                REQUIRE( below.size() == 2);
+                REQUIRE( above.size() == 1);
             }
         }
     }
