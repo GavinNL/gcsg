c612adf212315d3fcdb697fe84c4ca4730a938dc
Gavin <gavin.n.lobo@gmail.com>
2018-10-14 15:33:04 -0400

    Added python script to plot 2D shapes using matplotlib
    - plot_points.py inputfile
    - the inputfile is a ascii file which contains line segments on each line
    x1 y1 x2 y2
    x3 y3 x4 y4

--- plot_points.py
+++ plot_points.py
@@ -1,105 +1,34 @@
-import matplotlib.pyplot as plt
+#!/bin/env python3
 import numpy as np
+import pylab as pl
 import sys
+from matplotlib import collections  as mc
 
 
-#data = np.loadtxt( sys.argv , delimiter=",")
+array = []
+lines = []
+pointsx = []
+pointsy = []
 
+with open(sys.argv[1]) as f:
 
-#plt.plot([1,2,3,4], 'o')
-#plt.ylabel('some numbers')
+    for line in f: # read rest of lines
+       
+        array = [float(x) for x in line.split()]
+        lines.append( [ (array[0], array[1]), (array[2], array[3])   ])
+        pointsx.append( array[0] )
+        pointsx.append( array[2] )
+        pointsy.append( array[1] )
+        pointsy.append( array[3] )
 
+lc = mc.LineCollection(lines, linewidths=2)
+fig, ax = pl.subplots()
+ax.add_collection(lc)
+ax.autoscale()
+ax.margins(0.1)
+pl.xlabel('x')
+pl.ylabel('y')
 
-points = [(0,-1,0,0),
-(0,0,1,0),
-(-1,-1,-1,1),
-(0,-2,0,-1),
-(1,-2,0,-2),
-(1,-1,-1,-1),
-(2,-2,1,-2),
-(2,0,2,-2),
-(1,0,2,0),
-(1,1,1,-1),
-(-1,1,1,1)]
+pl.plot(pointsx, pointsy, 'o')
+pl.show()
 
-#points = [ (-1,0,2,0) ]
-import time
-import sys
-import matplotlib.pyplot as pp
-import random
- 
-if sys.platform == "win32":
-     # On Windows, the best timer is time.clock()
-     default_timer = time.clock 
-else:     
-    # On most other platforms the best timer is time.time()     
-    default_timer = time.time
-     
-     
-# generate ends for the line segments
-xpairs = []
-ypairs = []
-for p in points:
-    xends = [p[0], p[2]]
-    yends = [p[1], p[3]]
-    xpairs.append(xends)
-    ypairs.append(yends)
- 
-############################
-# time sequential call to matplotlib
-pp.figure()
-#pp.subplot(1,3,1)
- 
-t0 = default_timer()
-for xends,yends in zip(xpairs,ypairs):
-    pp.plot(xends,yends,'b-',alpha=0.1)
-t1 = default_timer()
- 
-pp.title('Sequential Plotting')
- 
-print( 'Execution time for sequential plotting = %f sec' % (t1-t0) )
-pp.show()
-sys.exit()
-############################
-# build argument list
-call_list = []
-for xends,yends in zip(xpairs,ypairs):
-    call_list.append(xends)
-    call_list.append(yends)
-    call_list.append('-b')
-     
-############################
-# time single call to matplotlib
-pp.subplot(1,3,2)
- 
-t0 = default_timer()
-pp.plot(*call_list,alpha=0.1)
-t1 = default_timer()
- 
-pp.title('Single Plot extended call')
- 
-print( 'Execution time for extended call plotting = %f sec' % (t1-t0) )
- 
-############################
-# rebuild ends using none to separate line segments
-xlist = []
-ylist = []
-for xends,yends in zip(xpairs,ypairs):
-    xlist.extend(xends)
-    xlist.append(None)
-    ylist.extend(yends)
-    ylist.append(None)
-     
-############################
-# time single call to matplotlib
-pp.subplot(1,3,3)
- 
-t0 = default_timer()
-pp.plot(xlist,ylist,'b-',alpha=0.1)
-t1 = default_timer()
- 
-pp.title('Single Plot Using None')
- 
-print( 'Execution time when using None = %f sec' % (t1-t0) )
- 
-pp.show()
