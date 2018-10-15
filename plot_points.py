f7faeda9de97d3ac7525dd393755d05c6f6054e7
Gavin <gavin.n.lobo@gmail.com>
2018-10-14 20:39:25 -0400

    plot_points.py now plots the normal vectors

--- plot_points.py
+++ plot_points.py
@@ -16,10 +16,27 @@ with open(sys.argv[1]) as f:
        
         array = [float(x) for x in line.split()]
         lines.append( [ (array[0], array[1]), (array[2], array[3])   ])
+
+        p1 = [array[0],array[1]]
+        p2 = [array[2],array[3]]
+
+        mx = 0.5*(p1[0]+p2[0])
+        my = 0.5*(p1[1]+p2[1])
+
+        vx = -(p2[1]-p1[1])
+        vy =   p2[0]-p1[0]
+
+        vx = vx/np.sqrt( vx*vx+vy*vy)
+        vy = vy/np.sqrt( vx*vx+vy*vy)
+        print(vx, vy)
         pointsx.append( array[0] )
         pointsx.append( array[2] )
         pointsy.append( array[1] )
         pointsy.append( array[3] )
+
+
+        lines.append( [ (mx,my), (mx+vx,my+vy) ] );
+
 
 lc = mc.LineCollection(lines, linewidths=2)
 fig, ax = pl.subplots()
