38a53c5d90b0ff0c64fcad4cca2262b88e8180b0
Gavin <gavin.n.lobo@gmail.com>
2018-10-14 20:39:12 -0400

    Added 2d example

---
+++ examples/2d.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <glm/glm.hpp>
+#include <array>
+#include <memory>
+#include<vector>
+#include <cstdint>
+
+#include<gcgs/hyperplane.h>
+#include<gcgs/line_segment.h>
+#include<gcgs/tree.h>
+
+#include <gcgs/geometry2d.h>
+
+using namespace std;
+
+void print_line_segments( gcgs::Tree<2, gcgs::line_segment> & T)
+{
+    using namespace gcgs;
+
+    T.for_each(
+    [&](line_segment & L)
+    {
+        printf("%f %f %f %f\n", L[0].x,L[0].y,L[1].x,L[1].y);
+    });
+}
+
+
+int main()
+{
+    using namespace gcgs;
+
+    Tree<2, line_segment> S1;
+    Tree<2, line_segment> S2;
+
+    Tree<2, line_segment> inside;
+    Tree<2, line_segment> outside;
+
+    auto B1 = geo2d::get_circle(1.0, glm::vec2(0.0), 20);//get_box(1,glm::vec2(0));
+    auto B2 = geo2d::get_box(1,glm::vec2(1,-1));
+
+    for(auto & L : B1)
+    {
+        S1.add(L);
+    }
+    for(auto & L : B2)
+    {
+        S2.add(L);
+    }
+
+    auto S3 = S1.Union(S2);
+    // would need to traverse S2 to get all the triangles instead
+    // of looping through B2.
+
+    S2.for_each(
+    [&](line_segment & L)
+    {
+        S1.partition(L, inside, outside);
+    });
+    S1.for_each(
+    [&](line_segment & L)
+    {
+        S2.partition(L, inside, outside);
+    });
+
+    print_line_segments(S3);
+
+    return 0;
+}
