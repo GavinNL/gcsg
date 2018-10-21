f7628ff6d9e3dc13efca46c6ffac633c4efd0dcf
Gavin <gavin.n.lobo@gmail.com>
2018-10-21 14:08:15 -0400

    Moved some of the unit test functions to a separate header

---
+++ test/src/geometry_tests.h
@@ -0,0 +1,65 @@
+#pragma once
+#ifndef GCSG_GEOMETRY_TESTS
+#define GCSG_GEOMETRY_TESTS
+
+#include <catch2/catch.hpp>
+#include<glm/glm.hpp>
+
+#include <gcgs/triangle.h>
+
+bool equal_points(glm::vec3 const &a, glm::vec3 const & b)
+{
+    return (Catch::Detail::Approx(a.x) == b.x) && (Catch::Detail::Approx(a.y) == b.y) && (Catch::Detail::Approx(a.z) == b.z);
+}
+
+// checks if two triangles are equal:
+// abc == bca == bca
+bool equal_triangles(gcgs::triangle const & abc, gcgs::triangle const & ABC)
+{
+    return
+    (equal_points( abc[0], ABC[0]) && equal_points( abc[1], ABC[1]) && equal_points( abc[2], ABC[2])) ||
+    (equal_points( abc[0], ABC[1]) && equal_points( abc[1], ABC[2]) && equal_points( abc[2], ABC[0])) ||
+    (equal_points( abc[0], ABC[2]) && equal_points( abc[1], ABC[0]) && equal_points( abc[2], ABC[1]));
+    //return (Catch::Detail::Approx(a.x) == b.x) && (Catch::Detail::Approx(a.y) == b.y) && (Catch::Detail::Approx(a.z) == b.z);
+}
+
+// returns true if b is in the triangle abc
+bool contains_point(gcgs::triangle const & abc, glm::vec3 const & b)
+{
+    return equal_points(abc[0], b) || equal_points(abc[1], b) || equal_points(abc[2], b);
+}
+
+// returns the index the point is located at
+int point_index(gcgs::triangle const & abc, glm::vec3 const & b)
+{
+    for(int i=0;i<3;i++)
+    {
+        if( equal_points(abc[i],b) )
+            return i;
+    }
+    return -1;
+}
+
+// given a triangle abc and two points from that triangle, return the third point
+glm::vec3 missing_point(gcgs::triangle const & abc, glm::vec3 const & a, glm::vec3 const & b)
+{
+    auto i = point_index(abc,a)+1;
+    auto j = point_index(abc,b)+1;
+
+    return abc[6 - (i+j) -1];
+
+}
+
+// two triangles are coplanar if their normals are the same
+bool are_coplanar(gcgs::triangle const & T1, gcgs::triangle const & T2)
+{
+    return glm::length(T1.get_hyperplane().normal() - T2.get_hyperplane().normal()) == Catch::Detail::Approx(0.0f);
+}
+
+// two triangles are antiplanar if their normals are negatives of each other
+bool are_antiplanar(gcgs::triangle const & T1, gcgs::triangle const & T2)
+{
+    return glm::length(T1.get_hyperplane().normal() + T2.get_hyperplane().normal()) == Catch::Detail::Approx(0.0f);
+}
+
+#endif
---
+++ test/src/unit-tree.cpp
@@ -0,0 +1,140 @@
+#include <catch2/catch.hpp>
+
+#include <string>
+#include <vector>
+
+#include <glm/glm.hpp>
+
+#include <gcgs/triangle.h>
+#include <gcgs/tree.h>
+
+#include "geometry_tests.h"
+
+SCENARIO( "Triangle Hyperplanes" ) {
+
+    GIVEN( "A line segment ab and ba" )
+    {
+        // on the xy plane
+        glm::vec3 a( 1 ,  0 , 0);
+        glm::vec3 b(-1 ,  1 , 0);
+        glm::vec3 c(-1 , -1 , 0);
+
+        gcgs::triangle abc{a,b,c};
+
+        gcgs::Tree<3, gcgs::triangle> T;
+
+        T.add(abc);
+
+        WHEN("Triangle ABC which is above abc is added to the plane")
+        {
+            // above the xy plane
+            glm::vec3 t( 0 ,  0 , 1);
+            gcgs::triangle ABC{a+t,b+t,c+t};
+
+            T.add( ABC );
+
+            THEN("The front subtree must be valid and the back subtree must not")
+            {
+                REQUIRE( T.m_root->m_front );
+                REQUIRE( !T.m_root->m_back );
+            }
+        }
+        WHEN("Triangle ABC which is below abc is added to the plane")
+        {
+            // above the xy plane
+            glm::vec3 t( 0 ,  0 , -1);
+            gcgs::triangle ABC{a+t,b+t,c+t};
+
+            T.add( ABC );
+
+            THEN("The front subtree must be valid and the back subtree must not")
+            {
+                REQUIRE( !T.m_root->m_front );
+                REQUIRE( T.m_root->m_back );
+            }
+        }
+    }
+}
+
+
+
+SCENARIO( "Quad Planes" ) {
+
+    using TreeType = gcgs::Tree<3, gcgs::triangle>;
+    GIVEN( "A line segment ab and ba" )
+    {
+        // on the xy plane
+        glm::vec3 a( 1 ,  0 , 0);
+        glm::vec3 b( 0 ,  0 , 0);
+        glm::vec3 c( 0 ,  0 , 1);
+        glm::vec3 d( 1 ,  0 , 1);
+
+        WHEN("Two triangles which are coplanar (in the xz plane) are added to an empty tree")
+        {
+            gcgs::triangle t1{a,c,b};
+            gcgs::triangle t2{a,d,c};
+
+            auto n1 = t1.get_hyperplane().normal();
+            auto n2 = t1.get_hyperplane().normal();
+
+            REQUIRE( equal_points(n1,n2));
+            REQUIRE( equal_points(n1, glm::vec3(0,-1,0)));
+
+            TreeType T;
+
+            T.add(t1);
+            T.add(t2);
+
+            THEN("Then T2 is considered behind T1")
+            {
+                REQUIRE( !T.m_root->m_front );
+                REQUIRE( T.m_root->m_back );
+                REQUIRE( T.m_root->count() == 2);
+            }
+
+            WHEN("A triangle which is in the xy plane and intersects the plane is added")
+            {
+                glm::vec3 A(  0 , -1 , 0);
+                glm::vec3 B(  2 ,  1 , 0);
+                glm::vec3 C( -2,   1 , 0);
+
+                gcgs::triangle ABC{A,B,C};
+
+                THEN("Triangle ABC is partioned by the tree")
+                {
+                    REQUIRE( TreeType::is_partitioned(T.m_root.get(), ABC) );
+                }
+                THEN("The partitioning triangle is t1")
+                {
+                    auto P = TreeType::partitioning_face(T.m_root.get(), ABC);
+
+                    REQUIRE( equal_triangles(P, t1));
+                }
+                WHEN("Triangle ABC is added to the tree")
+                {
+                    std::vector<gcgs::triangle> below, above;
+                    auto h1 = t1.get_hyperplane();
+
+                    ABC.split( h1, below, above);
+
+                    REQUIRE( below.size() == 2);
+                    REQUIRE( above.size() == 1);
+
+                    REQUIRE( below[0].surface_area() +
+                             below[1].surface_area()  +
+                             above[0].surface_area() == Approx(ABC.surface_area()));
+
+                    T.add(ABC);
+
+                    REQUIRE( T.m_root->count() == 5);
+                    REQUIRE( T.m_root->m_front->count() == 1);
+                    REQUIRE( T.m_root->m_back->count() == 3);
+                }
+
+            }
+
+        }
+
+
+    }
+}
